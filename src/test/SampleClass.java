86775884ac21f7bc4336498294ec58af1a30055b
Kalaivani23 <kalaivanivelusamy2302@gmail.com>
2019-09-27 18:06:12 +0530

    Sub

--- src/test/SampleClass.java
+++ src/test/SampleClass.java
@@ -3,5 +3,8 @@ package test;
 public class SampleClass {
 	public static void main(String[] args) {
 		System.out.println("Hi");
+		System.out.println("Sub");
+		int c=10-5;
+		System.out.println(c);
 	}
 }
