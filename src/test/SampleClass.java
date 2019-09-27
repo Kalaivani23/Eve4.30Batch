a074a9ab9bb58addcf4195e564b9b592d38d339a
Kalaivani23 <kalaivanivelusamy2302@gmail.com>
2019-09-27 17:55:18 +0530

    Addition

--- src/test/SampleClass.java
+++ src/test/SampleClass.java
@@ -3,5 +3,10 @@ package test;
 public class SampleClass {
 	public static void main(String[] args) {
 		System.out.println("Hi");
+		System.out.println("Add");
+		int a = 10;
+		int b = 20;
+		int c = a + b;
+		System.out.println(c);
 	}
 }
